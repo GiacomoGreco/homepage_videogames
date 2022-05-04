5eb2c29e4e61e48b467c3020c8d0305598b90980
Vale <valentino.torre@student.h-is.com>
2022-05-04 10:17:40 +0200

    started JS

---
+++ level.js
@@ -0,0 +1,47 @@
+// controllo delle variabili
+const donkey = document.querySelector('.monkey')
+let jumping = false
+
+const barrel1 = document.querySelector('.barrel')
+let moving1 = false
+
+let countBarrels = 0 
+const goal = 10
+const difficulty = .8
+// when the user press the spacebar
+document.onkeydown = (e) => {
+    console.log(e.code)
+    if (e.code == "Space"){
+        e.preventDefault()
+        if (jumping == false){
+            donkey.classList.add('jump')
+            jumping = true
+            setTimeout(() => {
+                donkey.classList.remove('jump')
+                jumping = false
+            }, 2000) 
+        }
+
+    }
+}
+
+
+
+//now detect when enemy start
+setInterval(() => {
+    const random = Math.random()
+    console.log(random)
+    if (random < difficulty && moving1 == false){
+        barrel1.classList.add('moving')
+        moving1 = true
+        setTimeout(() => {
+            barrel1.classList.remove('moving')
+            moving1 = false
+            countBarrels++
+            if (countBarrels == goal){
+                alert("You won")
+                location.href= window.destination
+            }
+        },window.enemyTiming) 
+    }
+},500)
